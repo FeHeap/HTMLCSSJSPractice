04708afe11fdaf32aeb2b9f41a8bdc57ee1c0671
Fe <82155803+FeHeap@users.noreply.github.com>
2021-07-06 23:24:08 +0800

    basic object start

---
+++ basicHTMLCSS/JS_BasicGrammar/object.js
@@ -0,0 +1,11 @@
+//built_in
+var str = '! ';
+console.log(str.length);
+
+
+//wrapper
+var flag = new Boolean(false); //Declaraing varible by Boolean constructor ,the object will be regarded as true.
+
+if(flag){
+    console.log('flag為true')
+} //output:flag為true
